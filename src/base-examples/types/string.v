aea64546f8719b621ac8c02eb8585ee887552075
xiaosanye <huhongyun@ykccn.com>
2023-04-24 20:13:47 +0800

    chore(string part end): string part end

--- src/base-examples/types/string.v
+++ src/base-examples/types/string.v
@@ -1,5 +1,7 @@
 module main
 
+const rocket = '🚀'
+
 fn main() {
 	name := 'Bob'
 	assert name.len == 3
@@ -28,4 +30,50 @@ fn main() {
 	println(star_str)
 	assert star_str == '\xe2\x98\x85'
 	// UTF-8 can be specified this way too.
+	s := 'hello 🌎' // emoji takes 4 bytes
+	assert s.len == 10
+
+	arr := s.bytes() // convert `string` to `[]u8`
+	assert arr.len == 10
+
+	s2 := arr.bytestr() // convert `[]byte` to `string`
+	assert s2 == s
+	s3 := '42'
+	n := s3.int() // 42
+	println(n)
+	// all int literals are supported
+	assert '0xc3'.int() == 195
+	assert '0o10'.int() == 8
+	assert '0b1111_0000_1010'.int() == 3850
+	assert '-0b1111_0000_1010'.int() == -3850
+	one_user := 'Bob'
+	println('Hello, ${one_user}!') // Hello, Bob!
+	age := 20
+	println('age = ${age}')
+	country := 'Netherlands'
+	println(country[0]) // Output: 78
+	println(country[0].ascii_str()) // Output: N
+	sss := r'hello\nworld' // the `\n` will be preserved as two characters
+	println(sss) // "hello\nworld"
+	println('can register = ${age > 13}')
+	println(rocket)
+	assert rocket.str() == '🚀'
+	println(rocket.bytes())
+	assert rocket.bytes() == [u8(0xf0), 0x9f, 0x9a, 0x80]
+	assert `\x61` == `a`
+	assert `\141` == `a`
+	assert `\u0061` == `a`
+
+	// multibyte literals work too
+	assert `\u2605` == `★`
+	assert `\u2605`.bytes() == [u8(0xe2), 0x98, 0x85]
+	assert `\xe2\x98\x85`.bytes() == [u8(0xe2), 0x98, 0x85]
+	assert `\342\230\205`.bytes() == [u8(0xe2), 0x98, 0x85]
+	rocket_string := '🚀'
+	assert rocket_string[0] != `🚀`
+	assert 'aloha!'[0] == `a`
+	hello := 'Hello World 👋'
+	hello_runes := hello.runes() // [`H`, `e`, `l`, `l`, `o`, ` `, `W`, `o`, `r`, `l`, `d`, ` `, `👋`]
+	println(hello_runes)
+	assert hello_runes.string() == hello
 }
