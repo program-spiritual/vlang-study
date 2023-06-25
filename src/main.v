c5fe6dedf7681d38d7b8cca1b762d167dd25fb7a
xiaosanye <huhongyun@ykccn.com>
2023-06-25 20:05:28 +0800

    refactor(module): refactor

--- src/main.v
+++ src/main.v
@@ -1,9 +1,20 @@
 module main
 
 import sample
+import methods
 
 fn main() {
 	println('Hello, World!')
 	info := sample.new_information('sample information')!
 	println(info)
+	user := methods.User{
+		age: 10
+	}
+
+	println(user.can_register())
+
+	user2 := methods.User{
+		age: 20
+	}
+	println(user2.can_register())
 }
