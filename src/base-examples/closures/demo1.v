3887d14a0f009492cb09a12dc695c3a6cdd1727c
xiaosanye <huhongyun@ykccn.com>
2023-07-14 18:09:30 +0800

    chore(func): closures

---
+++ src/base-examples/closures/demo1.v
@@ -0,0 +1,17 @@
+module main
+
+my_int := 1
+
+my_closure := fn [my_int] () {
+	println(my_int)
+}
+
+my_closure()
+
+func :=fn[i]() int{
+  return i
+}
+
+ println(func(1)==1)
+ i=123
+ println(func()==1)
