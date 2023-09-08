8eb3bd1abd9ccc91689a66768c63c0fa821ddbcb
xiaosanye <huhongyun@ykccn.com>
2023-09-08 20:13:18 +0800

    chore(atomic): atomic

--- src/main.v
+++ src/main.v
@@ -1,8 +1,9 @@
-module main
-
 import mymoudle2
+import v.vmod
 
 fn main() {
 	mymoudle2.say_hi()
 	mymoudle2.say_hi_and_bye()
+	vm := vmod.decode(@VMOD_FILE) or { panic(err) }
+	println('${vm.name} ${vm.version}\n ${vm.description}')
 }
